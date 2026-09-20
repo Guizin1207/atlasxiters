647fd1ca50e0bffb44c8d03a4cb2c1e58c6d75ef
Guizin1207 <guifelipemirandadasilva4@gmail.com>
2026-09-20 02:13:35 -0300

    Remove painel de pedidos de upgrade

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -13,7 +13,6 @@ import { MaintenanceCard } from "@/components/admin/MaintenanceCard";
 import { KeyGeneratorCard } from "@/components/admin/KeyGeneratorCard";
 import { KeysListCard } from "@/components/admin/KeysListCard";
 import { MessagesCard } from "@/components/admin/MessagesCard";
-import { UpgradeRequestsCard } from "@/components/admin/UpgradeRequestsCard";
 
 export default function AdminPage() {
   const { password, loading, signOut } = useAdmin();
@@ -83,7 +82,6 @@ export default function AdminPage() {
           <KeyGeneratorCard />
         </div>
 
-        <UpgradeRequestsCard />
 
         <KeysListCard />
 
